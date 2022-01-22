e78caf7145670dfd5991a1114446c9b321a77804
Ege Demirbas <30300963+Egerion@users.noreply.github.com>
2022-01-22 11:37:35 +0300

    Initial Commit - Spring Project Created!

---
+++ egedemirbas/src/main/java/com/loanapp/egedemirbas/EgedemirbasApplication.java
@@ -0,0 +1,13 @@
+package com.loanapp.egedemirbas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgedemirbasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgedemirbasApplication.class, args);
+	}
+
+}
---
+++ egedemirbas/src/test/java/com/loanapp/egedemirbas/EgedemirbasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loanapp.egedemirbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgedemirbasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
